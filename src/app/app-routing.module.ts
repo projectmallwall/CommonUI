09b6d8187045d1c9300702720eb1794f5c1b5041
Vinay Dhakre <vinaydhakre90@gmail.com>
2019-12-23 08:28:45 +0000

    Merge from develop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { BodyComponent } from './renderer/body/body.component';
 import { HeaderComponent } from './renderer/header/header.component';
 import { NavBarComponent } from './renderer/nav-bar/nav-bar.component';
 import { FooterComponent } from './renderer/footer/footer.component';
+import { SectionComponent } from './section/section/section.component';
 
 
 const routes: Routes = [{
   path: '',
   component: HeaderComponent,
   outlet: 'header'
+}, {
+  path: 'section',
+  component: SectionComponent
 }, {
   path: '',
   component: NavBarComponent,
--- src/app/section/section/section.component.ts
+++ src/app/section/section/section.component.ts
@@ -6,10 +6,97 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section.component.scss']
 })
 export class SectionComponent implements OnInit {
+  structuralData: any = {
+        id: "section id is compulsory for form",
+        type: "content/form",
+        col: {
+          count: 3,
+          ratio: {
+            _comment: "Sould add up in multiple of 12 only.",
+            lg: "6:3:3",
+            md: "6:3:3",
+            sm: "12:6:6",
+            xs: "12:12:12"
+          },
+          autoPlace: "true/false"
+        },
+        fieldList: [{
+            name: "customerName",
+            type: "text",
+            accessor: "customer.name"
+          }, {
+            name: "address",
+            type: "textArea",
+            accessor: "customer.address"
+          }, {
+            name: "gender",
+            type: "radio",
+            accessor: "customer.gender"
+          },
+          {
+            name: "save",
+            type: "submit/cancel/reset",
+            formIds: ["section1", "section12"]
+          }]
+      };;
+  /*columnStructure: {
+    noOfColumn: number,
+    classes: {
+      xs: Array<string>,
+      sm: Array<string>,
+      md: Array<string>,
+      lg: Array<string>
+    }
+  };*/
+  columnStructure: any = {};
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.breakColumnForDevices();
+  }
+
+  private convertStringIntoNumber(value: string): number {
+    let num: number = Number(value);
+    if (Number.isNaN(num)) {
+      console.log('section ratio number issue');
+    }
+    return num;
+  }
+
+  private convertColumnRatioIntoClass(arr: Array<string>, type: string): Array<string> {
+    let resultArr: Array<string> = [];
+    for (let index: number = 0; index < arr.length; index++) {
+      resultArr[index] = "col-" + type + arr[index];
+    }
+
+    return resultArr;
+  }
+
+  private checkForRequiredParameters(): void {
+
+  }
+
+  private breakColumnForDevices(): void {
+    this.checkForRequiredParameters();
+    this.columnStructure.noOfColumn = this.convertStringIntoNumber(this.structuralData.col.count);
+    this.columnStructure.classes = {};
+    if (this.structuralData.col.ratio.xs) {
+      this.columnStructure.classes.xs = this.convertColumnRatioIntoClass(this.structuralData.col.ratio.xs.split(':'), 'xs');
+    }
+
+    if (this.structuralData.col.ratio.sm) {
+      this.columnStructure.classes.sm = this.convertColumnRatioIntoClass(this.structuralData.col.ratio.sm.split(':'), 'sm');
+    }
+
+    if (this.structuralData.col.ratio.md) {
+      this.columnStructure.classes.md = this.convertColumnRatioIntoClass(this.structuralData.col.ratio.md.split(':'), 'md');
+    }
+
+    if (this.structuralData.col.ratio.lg) {
+      this.columnStructure.classes.lg = this.convertColumnRatioIntoClass(this.structuralData.col.ratio.lg.split(':'), 'lg');
+    }
   }
 
 }
