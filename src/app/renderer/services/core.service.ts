bf12258c88af98daa22181c5b94a05f56e019c75
projectmallwall <projectmallwall@gmail.com>
2019-12-24 09:58:39 +0000

    Moved the page configuration fetching code to core service.

--- src/app/renderer/services/core.service.ts
+++ src/app/renderer/services/core.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { CommonService } from 'src/app/common.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoreService {
 
-  constructor() { }
+  constructor(private commonService: CommonService, private httpClient: HttpClient) { }
+
+  fetchPageStructuralData(url): Observable<any> {
+    let urlParts = this.commonService.splitUrl(url);
+    let jsonFileName = '';
+    urlParts.forEach(function(urlString){
+      jsonFileName += urlString + '.';
+    });
+    if (jsonFileName !== "") {
+      jsonFileName += 'section.json';
+      return this.httpClient.get('./assets/pageLayout/'+jsonFileName);
+    } else {
+      return throwError('Structural configuration for this page does not exist.');
+    }
+    console.log('JSON file name:'+jsonFileName);
+  }
 }
