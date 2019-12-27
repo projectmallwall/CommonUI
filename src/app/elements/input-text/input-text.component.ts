be261c15b8958c95584ffd312e83bd3cfb60729b
prashantrenge <prashantrenge@gmail.com>
2019-12-27 08:25:43 +0000

    Added prototype char, radio, check box widgets

--- src/app/elements/input-text/input-text.component.ts
+++ src/app/elements/input-text/input-text.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { ViewElement } from '../view-element.interface';
+import {CharField } from '../../form/interfaces/char-field';
+import { spawn } from 'child_process';
+
 
 @Component({
-  selector: 'app-input-text',
+  selector: 'char-input-text',
   templateUrl: './input-text.component.html',
   styleUrls: ['./input-text.component.scss']
 })
-export class InputTextComponent implements ViewElement, OnInit {
+export class CharTextComponent implements ViewElement, OnInit {
 
-  @Input() data: String;
-  @Input() span: Number;
-  @Input() editMode: Boolean = false;
-
+  @Input() field:  CharField;
+  @Input() data: any;
+  @Input() editMode: boolean;
   @Output('dataChange') private _dataChange = new EventEmitter<String>();
 
   public dataChange(): EventEmitter<String> {
     return this._dataChange;
   }
 
+  public eventAction($event) {
+    if (event.type === 'focus') {
+
+    } else if (event.type === 'focusout') {
+
+    } else if (event.type === 'click') {
+
+    }
+  }
+
   constructor() { }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
   }
+
 
 }
