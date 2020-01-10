d79495a439de62234dff0be3b496e725b1ef5216
prashantrenge <prashantrenge@gmail.com>
2020-01-10 08:51:14 +0000

    Added Button and field object

--- src/app/elements/button/button.component.ts
+++ src/app/elements/button/button.component.ts
@@ -2,18 +2,18 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { ViewElement } from '../view-element.interface';
 import {Button } from '../../form/interfaces/button';
 
+
 @Component({
   selector: 'cui-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.sass'],
-});
-
+})
 export class ButtonComponent implements ViewElement, OnInit {
 
-  @Input() field:  Button;
-  @Input() data: any;
+  @Input() fieldObj:  Button;
   @Input() editMode: boolean;
   @Output('dataChange') private _dataChange = new EventEmitter<String>();
+  data: any;
 
   public dataChange(): EventEmitter<String> {
     return this._dataChange;
